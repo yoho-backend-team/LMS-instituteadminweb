5d92aca4a893c294bb693aaad4d7000bb9a19e49
suryas-1805 <suryas18051998@gmail.com>
2025-07-30 11:14:11 +0530

    changes

--- src/pages/Class Management/Live Classes/LiveClasses.tsx
+++ src/pages/Class Management/Live Classes/LiveClasses.tsx
@@ -3,7 +3,7 @@ import { COLORS, FONTS } from '../../../constants/uiConstants';
 const LiveClasses = () => {
 	return (
 		<>
-			<div className=''>
+			<div className='my-4'>
 				<div>
 					<h1 style={{ ...FONTS.heading_04_bold, color: COLORS.gray_dark_01 }}>
 						Live Classes
