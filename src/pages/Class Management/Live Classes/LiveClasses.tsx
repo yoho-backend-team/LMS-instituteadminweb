55ece61437dcb939ab520cb9ae634e3f2b8632b2
suryas-1805 <suryas18051998@gmail.com>
2025-07-30 12:04:36 +0530

    changes

--- src/pages/Class Management/Live Classes/LiveClasses.tsx
+++ src/pages/Class Management/Live Classes/LiveClasses.tsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
 import { COLORS, FONTS } from '../../../constants/uiConstants';
+import { Button } from '../../../components/ui/button';
+import filter from '../../../assets/Filter.png';
+import plus from '../../../assets/Add.png';
 
 const LiveClasses = () => {
+	const [showFilter, setShowFilter] = useState(false);
+	const [showCreateModal, setShowCreateModal] = useState(false);
+
 	return (
 		<>
-			<div className='my-4'>
-				<div>
-					<h1 style={{ ...FONTS.heading_04_bold, color: COLORS.gray_dark_01 }}>
+			<div className='my-4 min-h-screen'>
+				<div className='mb-8'>
+					<h2
+						className=' mb-6'
+						style={{ ...FONTS.heading_04_bold, color: COLORS.gray_dark_01 }}
+					>
 						Live Classes
-					</h1>
+					</h2>
+
+					<div className='flex justify-between items-center'>
+						<Button
+							onClick={() => setShowFilter(!showFilter)}
+							className='bg-[#1BBFCA] hover:bg-[#1BBFCA]  px-4 flex items-center gap-2'
+							style={{ ...FONTS.heading_03 }}
+						>
+							<img src={filter} className='w-4 h-4' />
+							{showFilter ? 'Hide Filter' : 'Show Filter'}
+						</Button>
+
+						<Button
+							className='bg-[#1BBFCA] hover:bg-[#1BBFCA]  px-4 flex items-center gap-2'
+							style={{ ...FONTS.heading_03 }}
+							onClick={() => setShowCreateModal(true)}
+						>
+							<img src={plus} className='w-4 h-4' />
+							Add New Batch
+						</Button>
+					</div>
 				</div>
 			</div>
 		</>
