5c86faf5d190c38ad90209be3d1451a0fef4f6a3
ARAVINTH-12345 <aravinth.yoho@gmail.com>
2025-07-31 19:15:21 +0530

    completed

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,22 +1,21 @@
+
 import { Outlet } from 'react-router-dom';
-import backgroundimage from '@/assets/Backgroundimage.png';
 import { useState } from 'react';
 import Navbar from '../components/shared/Navbar';
 import SideBar from '../components/shared/SideBar';
 
 export const MainLayout = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-	
 
     const sidebarWidth = isSidebarOpen ? 250 : 87;
 
     return (
-    <div className='flex flex-col h-screen w-screen overflow-hidden bg-[#1BBFCA]'>
+        <div className='flex flex-col h-screen w-screen overflow-x-hidden bg-[#1BBFCA]'>
             <div className=''>
                 <Navbar />
             </div>
 
-            <div className='flex  '>
+            <div className='flex overflow-y-auto '>
                 <div
                     className='flex  h-[calc(100vh-64px)] transition-all duration-300 '
                     style={{ width: `${sidebarWidth}px` }}
@@ -24,9 +23,7 @@ export const MainLayout = () => {
                     <SideBar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
                 </div>
 
-                <div
-                    className='flex-1 overflow-auto p-6 bg-white rounded-tl-[35px]'
-                >
+                <div className='flex-1 overflow-auto p-6 bg-white rounded-tl-[35px]'>
                     <Outlet />
                 </div>
             </div>
