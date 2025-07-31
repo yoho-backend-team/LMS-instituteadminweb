3dde4cfc8dfa24be56ba8608a17913c0c0da37ae
Vignesh <smartvignesh81245@email.com>
2025-07-31 16:28:10 +0530

    completed

--- src/components/staff/Securitypage.tsx
+++ src/components/staff/Securitypage.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import { COLORS, FONTS } from "../../constants/uiConstants";
 
 const Securitypage: React.FC = () => {
   return (
-    <div className="bg-white shadow-md rounded-lg p-6 w-full max-w-5xl mx-auto mt-10">
+    <div className="bg-white rounded-xl border border-gray-100 transition-shadow duration-200 shadow-[0_0_15px_rgba(0,0,0,0.1)] hover:shadow-[0_0_20px_rgba(0,0,0,0.15)] p-6 w-full max-w-5xl mx-auto mt-10">
       <h2 className="text-lg font-semibold text-gray-800 mb-6">Change Password</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-        <div>
-          <label className="block text-sm text-gray-700 mb-1">New Password</label>
-          <Input type="password" placeholder="********" />
+        <div  style={{...FONTS.heading_08,color:COLORS.gray_dark_02}} >
+          <label className="mb-1">New Password</label>
+          <Input className="border border-[#716F6F]" type="password" placeholder="********" />
         </div>
-        <div>
+        <div style={{...FONTS.heading_08,color:COLORS.gray_dark_02}} >
           <label className="block text-sm text-gray-700 mb-1">Confirm New Password</label>
-          <Input type="password" placeholder="********" />
+          <Input className="border border-[#716F6F]" type="password" placeholder="********" />
         </div>
       </div>
       <div className="flex justify-end">
