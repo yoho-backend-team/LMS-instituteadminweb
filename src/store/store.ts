c2bb49da80dc36c4679ce90b1188470cc4dbc50b
suryas-1805 <suryas18051998@gmail.com>
2025-07-31 17:30:55 +0530

    changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import staffattendance from '../features/teachingstaffAttendance/slice'
+import { configureStore } from '@reduxjs/toolkit';
+import staffattendance from '../features/teachingstaffAttendance/slice';
 
 const store = configureStore({
-    reducer: {
-        staffAttendace: staffattendance,
-    }
-})
+	reducer: {
+		staffAttendace: staffattendance,
+	},
+});
 
-export default store
+export default store;
