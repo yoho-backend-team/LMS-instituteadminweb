c21f989d1933d859e3cd59c6f9efd557af364daf
Prakash-yoho <prakash.yohotechnologies@gmail.com>
2025-08-01 18:38:10 +0530

    Dashboard Store

--- src/apis/httpEndpoints.ts
+++ src/apis/httpEndpoints.ts
@@ -4,7 +4,7 @@ import { GetLocalStorage } from "../utils/localStorage";
 
 const getInstituteDetails = () => {
     if (typeof (secureLocalStorage) !== "undefined") {
-        const institute = GetLocalStorage("institute")
+        const institute = GetLocalStorage("instituteId")
         return institute
     } else {
         return null
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import staffattendance from '../features/teachingstaffAttendance/slice';
 import IdcardSlice from '../features/StudentIdCard/reducers/IdcardSlice';
 import StaffIDcardSlice from '../features/StaffIdCard/reducers/IdcardSlice';
+import DashboardSlice from '../features/Dashboard/reducers/DashboardSlice';
 
 const store = configureStore({
 	reducer: {
 		staffAttendace: staffattendance,
 		IdcardSlice:IdcardSlice,
 		StaffIDcardSlice:StaffIDcardSlice,
+		DashboardSlice:DashboardSlice,
 	},
 });
 
