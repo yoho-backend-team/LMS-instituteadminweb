6bf243a9eb099aa1b4f85097738d7ff7e9d5e455
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2025-08-02 18:04:30 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Toaster } from 'react-hot-toast';
 import AppRoutes from './routes/AppRoutes';
 import { AuthProvider } from './pages/Auth/AuthContext';
 import { TicketProvider } from './components/StudentTickets/TicketContext';
-import { Provider } from 'react-redux'
-import store from './store/store';
 
 function App() {
 	return (
@@ -40,35 +38,6 @@ function App() {
 					/>
 					</TicketProvider>
 				</AuthProvider>
-				<Provider store={store}>
-					<AuthProvider>
-						<AppRoutes />
-						<Toaster
-							position='top-right'
-							toastOptions={{
-								duration: 4000,
-								style: {
-									background: '#363636',
-									color: '#fff',
-								},
-								success: {
-									duration: 3000,
-									style: {
-										background: '#10B981',
-										color: '#fff',
-									},
-								},
-								error: {
-									duration: 4000,
-									style: {
-										background: '#EF4444',
-										color: '#fff',
-									},
-								},
-							}}
-						/>
-					</AuthProvider>
-				</Provider>
 			</BrowserRouter>
 		</>
 	);
