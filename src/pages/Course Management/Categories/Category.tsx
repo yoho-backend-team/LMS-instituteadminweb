f7bd11a7db7fc3c02cdcf13511382a7bd95c85d9
vinutha <vinuthaarul.yohotechno@gmail.com>
2025-08-02 20:39:59 +0530

    update

--- src/pages/Course Management/Categories/Category.tsx
+++ src/pages/Course Management/Categories/Category.tsx
@@ -290,12 +290,9 @@ export const DashboardCards: React.FC = () => {
         <h2 className="text-2xl font-semibold mb-4">Admin User</h2>
         <div className="flex justify-between items-center">
           <button 
-            className="px-4 py-2 bg-[#1BBFCA] text-white rounded hover:bg-[#1BBFCA] flex items-center"
+            className="px-4 py-2 bg-[#1BBFCA] text-white rounded hover:bg-[#1BBFCA]"
             onClick={() => setShowFilter(!showFilter)}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
-            </svg>
             {showFilter ? 'Hide' : 'Show Filter'}
           </button>
           <button 
