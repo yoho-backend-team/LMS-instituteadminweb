766d8e6f6e9581a7c142d560d6ae4b6ada75fb0b
Sathyajothi-05-03 <sathyajothi.yoho@gmail.com>
2025-08-04 09:49:00 +0530

    tick

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -49,7 +49,6 @@ import NotificationPage from '../pages/Notification/NotificationPage';
 import Accountpf from '../components/Profile/AccProfile';
 import EditUserInfo from '../components/Profile/EditUserInfo';
 import TicketDetailsPage from '../pages/Ticket Management/Student/TicketDetailsPage';
-import { TicketProvider } from '../components/StudentTickets/TicketContext';
 import AddAttendance from '../pages/Attendance Management/Staffs Attendance/AddAttendance';
 import StudentCertificate from '../pages/Certificate Management/Student Certificate/StudentCertificate';
 import CertificateView from '../components/cerificateManagement/certificateView';
@@ -79,7 +78,7 @@ const AppRoutes = () => {
 				<Route index element={<Dashboard />} />
 				{/* Profile Management */}
 
-				<Route path="/tickets/:id" element={<TicketProvider><TicketDetailsPage /></TicketProvider>} />
+				<Route path="/tickets/:id" element={<TicketDetailsPage />} />
 
 				<Route path="noti/msg" element={<NotificationPage />} />
 
