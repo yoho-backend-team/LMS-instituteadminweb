42b2fe6cf1daf853dc316733dba1cc098e89014e
suruthiga407! <suruthiga.yaho@gmail.com>
2025-08-04 18:49:33 +0530

    msh

--- src/features/Class Management/offlineClass/services/index.tsx
+++ src/features/Class Management/offlineClass/services/index.tsx
@@ -30,3 +30,51 @@ export const updateOfflineClass = async (data:any)=>{
         console.log (error)
     }
 }
+
+export const createOfflineClass = async (data:any)=>{
+    try{
+        const response = await Client.offline_class.create(data)
+   if(response){
+    return response ;
+   }
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+export const getAllBranches = async (params:any)=>{
+    try{
+        const response = await Client.branch.getAll(params)
+   if(response){
+    return response 
+   }
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+export const getAllBatches = async (params: any) => {
+    try {
+        const response = await Client.batch.getWithId(params);
+        if (response) {
+            return response;
+        }
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
+
+export const getAllCourses = async (params: any) => {
+    try {
+        const response = await Client.staff.getCourse(params);
+        if (response) {
+            return response;
+        }
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+};
